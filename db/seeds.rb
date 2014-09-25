d58ca536f85d8b29eb97fce896c0ff440f1f7218
Dave Nelson <davidicus21@gmail.com>
2014-09-25 12:29:47 -0600

    made better seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
-Menu.create(name: "Texas Roadhouse", owner: "Dave")
-
-MenuItem.create(name: "Ribeye", price: 20.00, description: "A 20 oz. cut of our most flavorful steak served with our special peppercorn sauce and two sides (1110 cal.)", menu_id: 1)
-MenuItem.create(name: "Salad", price: 7.00, description: "Crisp hearts of romaine, fresh parmesan cheese, and made-from-scratch croutons, tossed with our zesty Caesar dressing (770 cal.)", vegetarian: true, menu_id: 1)
-
-Menu.create(name: "Chunga's", owner: "Dave")
-
-100.times do
-  menu = Menu.create(name: Faker::Company.name, owner: Faker::Name.name)
+def make_items(id)
   10.times do
-    MenuItem.create(name: Faker::Lorem.word, price: Random.rand(11), description: Faker::Lorem.sentence, vegetarian: [true, false].sample, menu_id: menu.id)
+    MenuItem.create(name: Faker::Lorem.word, price: Random.rand(11), description: Faker::Lorem.sentence, vegetarian: [true, false].sample, menu_id: id)
   end
+end
+
+menus = ["Texas Roadhouse", "Chunga's", "Olive Garden", "Spitz", "Cafe Rio", "Cafe Zupas",
+"Costa Vida", "JDawgs", "Cubby's", "Smash Burger", "In'N'Out Burger", "Apollo Burger", "China Garden"]
+
+menus.each do |restaurant|
+  menu = Menu.create(name: restaurant, owner: ["Dave", "Ken", "Jeremy", "Carrie"].sample)
+  make_items(menu.id)
 end
