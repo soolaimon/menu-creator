fe841cc00c8caaf2c3a06793b047246dbfb2db82
Dave Nelson <davidicus21@gmail.com>
2014-09-26 18:28:55 -0600

    added vegetarian filter

--- app/assets/javascripts/angular/controllers/menuCtrl.js
+++ app/assets/javascripts/angular/controllers/menuCtrl.js
@@ -3,13 +3,25 @@ menuCreator.controller('menuCtrl', ['$scope', '$http', function ($scope, $http) 
   $scope.search = function (query) {
     $http.get('/search', { params: {q: query}})
       .success(function(menus){
-        $scope.menus = menus
+        $scope.menus = menus;
       })
     }
 
-    $scope.setMenu = function (menu) {
-      $scope.selectedMenu = menu;
+  $scope.menuFilters = {};
+
+  $scope.toggleFilter = function (filter) {
+    if ($scope.menuFilters.hasOwnProperty(filter)){
+      delete $scope.menuFilters[filter];
+    } else {
+      $scope.menuFilters[filter] = true;
     }
+  }
+
+  $scope.setMenu = function (menu) {
+    $scope.selectedMenu = menu;
+  };
+
+
 
 
 }]);
